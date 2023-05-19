e244853c68b1b1c87ae2ffa2a3b3157aee296adc
kyrieCCC <1186624948@qq.com>
2023-05-19 15:05:18 +0800

    feat: middleware登录中间件

--- app/router.js
+++ app/router.js
@@ -4,8 +4,10 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
+  const { router, controller, middleware } = app;
+  const _jwt = middleware.jwtErr(app.config.jwt.secret)
+
+  router.get('/', controller.home.index);   
   //添加一个路由
   router.post('/add', controller.home.add);
   router.get('/user', controller.home.user)
@@ -14,5 +16,5 @@ module.exports = app => {
   router.post('/deleteUser', controller.home.deleteUser);
   router.post('/user/register', controller.user.register);
   router.post('/user/login', controller.user.login);
-  router.get('/user/test', controller.user.test_token);
+  router.get('/user/test', _jwt, controller.user.test_token);
 };
